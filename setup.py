2174a789f6d540470e958af0608532b6b47ef4e2
Gaea <gaea@lahacker.net>
2020-10-27 01:44:56 +0000

    Bootstrap

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ from setuptools import setup
 
 setup(requires=["acme_tiny", "BeautifulSoup4", "cssselect", "gevent",
                 "html5lib", "httpagentparser", "lxml", "mf2py", "mf2util",
-                "networkx", "pendulum", "pycrypto", "python_mimeparse",
-                "pyvirtualdisplay", "PySocks", "regex", "requests",
+                "networkx", "pendulum", "pycrypto", "pyscreenshot", "PySocks",
+                "python_mimeparse", "pyvirtualdisplay", "regex", "requests",
                 "selenium", "stem", "unidecode", "uwsgi", "watchdog"],
       provides={"pygments.styles": ["Lunar = solarized:Lunar",
                                     "Solar = solarized:Solar"],
