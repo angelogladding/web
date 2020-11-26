ffd5aa261ddd06de029193611715d3115877158b
Angelo Gladding <https://angelogladding.com>
2020-11-26 01:05:19 +0000

    Bootstrap

--- web/framework/letsencrypt.py
+++ web/framework/letsencrypt.py
@@ -16,6 +16,6 @@ def generate_cert(domain, cert_dir, challenge_dir):
     if not csr.exists():
         sh.openssl("req", "-new", "-sha256", "-key", private_key, "-subj",
                    f"/CN={domain}", _out=str(csr))
-    with (cert_dir / "chain.crt").open("w") as fp:
+    with (cert_dir / "domain.crt").open("w") as fp:
         fp.write(acme_tiny.get_crt(account_key, csr, challenge_dir))
     csr.unlink()
