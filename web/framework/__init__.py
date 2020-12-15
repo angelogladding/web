871fb3af7755e899c70fd1c709008046610a51c2
Angelo Gladding <angelogladding.com>
2020-12-15 01:48:56 +0000

    Bootstrap

--- web/framework/__init__.py
+++ web/framework/__init__.py
@@ -911,7 +911,7 @@ class Application:
             tx.response.status = "200 OK"
             tx.response.headers.content_type = content_types[asset.suffix]
             # header("Content-Type", content_types[asset.suffix])
-            return content
+            return [content]
 
         try:
             tx.host._contextualize(self, tx.request.headers.host.name,
