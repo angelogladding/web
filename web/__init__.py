5255530be8b0172478ccee114d1add9280e4eaaf
Angelo Gladding <angelogladding.com>
2020-12-18 20:54:39 +0000

    Bootstrap

--- web/__init__.py
+++ web/__init__.py
@@ -14,6 +14,7 @@ Simple interface, simple deploy.
 # TODO clean these up
 from hstspreload import in_hsts_preload
 import mf
+from mkdn import render as mkdn
 import mm
 from mm import Template as template  # noqa
 from mm import templates  # noqa
@@ -32,7 +33,7 @@ from .response import (Status,  # noqa
                          MethodNotAllowed, Conflict, Gone)
 from .tasks import run_queue
 
-__all__ = ["in_hsts_preload", "mf", "mm", "template", "templates", "pendulum",
-           "indieauth", "micropub", "microsub", "webmention", "websub",
-           "run_queue", "Created", "ConnectionError"]
+__all__ = ["in_hsts_preload", "mf", "mkdn", "mm", "template", "templates",
+           "pendulum", "indieauth", "micropub", "microsub", "webmention",
+           "websub", "run_queue", "Created", "ConnectionError"]
 __all__ += agent.__all__ + framework.__all__
